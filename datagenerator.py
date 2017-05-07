00b4cd1af6b1502a1519c8ad8cd0b57cbbe79255
Tyler Roesler <troesler95@gmail.com>
2017-05-06 22:06:34 -0400

    added a bunch more false pos

--- datagenerator.py
+++ datagenerator.py
@@ -3,16 +3,16 @@ import os
 import numpy as np
 
 datagen = ImageDataGenerator(
-    rotation_range=40,
-    width_shift_range=0.2,
-    height_shift_range=0.2,
-    shear_range=0.2,
-    zoom_range=0.2,
+    rotation_range=20,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    shear_range=0.1,
+    zoom_range=0.1,
     horizontal_flip=False,
     fill_mode='nearest'
 )
 
-warn_path = os.path.join(os.getcwd(), 'warnClassification')
+warn_path = os.path.join(os.getcwd(), 'falsePositives')
 
 for image in os.listdir(warn_path):
     img = load_img(os.path.join(warn_path, image))
@@ -22,7 +22,7 @@ for image in os.listdir(warn_path):
 
     i=0
     for batch in datagen.flow(x, batch_size=1,
-                              save_to_dir='warnClassification', save_format='jpeg'):
+                              save_to_dir='falsePositives', save_format='jpeg'):
         i+=1
-        if i > 20:
+        if i > 5:
             break
