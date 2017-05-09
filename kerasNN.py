63e07aff3bee2dbb7cd434241dc89b35a0c72b1a
Tyler Roesler <troesler95@gmail.com>
2017-05-09 11:30:20 -0400

    some finishing touches

--- kerasNN.py
+++ kerasNN.py
@@ -115,7 +115,7 @@ model.add(Dropout(0.3))
 model.add(Flatten())
 model.add(Dense(128))
 model.add(Activation('relu'))
-model.add(Dropout(0.5))
+model.add(Dropout(0.2))
 model.add(Dense(2))
 model.add(Activation('softmax'))
 
